3807e78c95059c5e10233c43ead9408d4d669a37
fafa1971 <fafa1971@4d036993-bc84-4201-9156-cab7e310e232>
2007-09-08 23:13:59 +0000

    Now supports also Virtex5 devices

--- tools/src/build_xst.cmd
+++ tools/src/build_xst.cmd
@@ -6,5 +6,12 @@ run
 -top s1_top
 -opt_mode SPEED
 -opt_level 1
--p xc3s500e-fg320
+# Device used in Spartan3E Starter Kit
+# -p xc3s500e-fg320
+# Biggest Spartan3E device
+# -p xc3s1600e-fg320
+# Biggest Virtex4 device supported by ISE WebPack
+# -p xc4vlx25-ff668
+# Biggest Virtex5 device supported by ISE WebPack
+-p xc5vlx30-ff676
 -vlgincdir { $S1_ROOT/hdl/rtl/sparc_core/include $S1_ROOT/hdl/rtl/s1_top }
