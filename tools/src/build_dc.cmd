2055565dadf9ad8242b0b7b0ec445b5766181813
fafa1971 <fafa1971@4d036993-bc84-4201-9156-cab7e310e232>
2007-10-24 21:50:55 +0000

    Modified to use XG syntax

--- tools/src/build_dc.cmd
+++ tools/src/build_dc.cmd
@@ -7,13 +7,13 @@ uniquify
 /* check_design */
 
 create_clock -period 2.0 -name sys_clock_i find(port, "sys_clock_i")
-set_input_delay  1 -max -clock sys_clock_i all_inputs() - find(port, "sys_clock_i")
+set_input_delay  -max -clock sys_clock_i 1 all_inputs () find (port, "sys_clock_i")
 set_output_delay 1 -max -clock sys_clock_i all_outputs()
 
 compile -map_effort high
 
-write -format db -hierarchy -output s1_top.db
-write -format verilog -hierarchy -output s1_top.v
+write -output s1_top.db  -format ddc  -hierarchy
+write -output s1_top.v  -format verilog  -hierarchy
 
 report_area > report_area.txt
 report_timing > report_timing.txt
