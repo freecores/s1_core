6ca3dcbf09bc5d2faa70ff6e9fddfadd57a56603
fafa1971 <fafa1971@4d036993-bc84-4201-9156-cab7e310e232>
2008-03-03 15:00:32 +0000

    Decreased clock frequency from 250 to 200 MHz.

--- tools/src/build_dc.cmd
+++ tools/src/build_dc.cmd
@@ -13,9 +13,9 @@ check_design
 
 # Constraints and mapping on target library
 
-create_clock -period 4.0 -waveform [list 0 2.0] sys_clock_i
-set_input_delay 2.0 -clock sys_clock_i -max [all_inputs]
-set_output_delay 1.0 -clock sys_clock_i -max [all_outputs]
+create_clock -period 5.0 -waveform [list 0 2.5] sys_clock_i
+set_input_delay  2.0 -clock sys_clock_i -max [all_inputs]
+set_output_delay 2.0 -clock sys_clock_i -max [all_outputs]
 set_dont_touch_network [list sys_clock_i sys_reset_i]
 set_drive 0 [list sys_clock_i sys_reset_i]
 set_wire_load_mode enclosed
