3ae493e1789a2e37bff2a148c38464d8ca685011
fafa1971 <fafa1971@4d036993-bc84-4201-9156-cab7e310e232>
2008-03-10 10:43:35 +0000

    Again, used module names instead than instance names in bottom-up synthesis approach.

--- tools/src/build_dc.cmd
+++ tools/src/build_dc.cmd
@@ -2,38 +2,43 @@
 # The Tcl script under $S1_ROOT/tools/src/build_dc.cmd is attached at the end of the filelist for DC;
 # if you modify this file *REMEMBER* to run 'update_filelist' or you'll run the old version!!!
 
-# Technology-independent elaboration and linking
+# Variables setting
 
-set active_design s1_top
-elaborate $active_design
-current_design $active_design
-link
-uniquify
-check_design
+set sub_modules {ifu lsu exu ffu mul spu tlu s1_top}
+set sub_clocks  {rclk clk sys_clock_i}
+set sub_resets  {grst_l arst_l sys_reset_i}
 
-# Constraints and mapping on target library
+foreach active_design $sub_modules {
 
-create_clock -period 5.0 -waveform [list 0 2.5] sys_clock_i
-set_input_delay  2.0 -clock sys_clock_i -max [all_inputs]
-set_output_delay 2.0 -clock sys_clock_i -max [all_outputs]
-set_dont_touch_network [list sys_clock_i sys_reset_i]
-set_drive 0 [list sys_clock_i sys_reset_i]
-set_wire_load_mode enclosed
-set_max_area 0
-set_fix_multiple_port_nets -buffer_constants -all
-compile
+  # Technology-independent elaboration and linking
+  elaborate      $active_design
+  current_design $active_design
+  link
+  uniquify -dont_skip_empty_designs
 
-# Export the mapped design
+  # Set constraints and mapping on target library
+  create_clock -period 5.0 -waveform [list 0 2.5] [get_ports $sub_clocks]
+  set_input_delay  1.8 -clock [get_clocks $sub_clocks] -max [all_inputs]
+  set_output_delay 1.2 -clock [get_clocks $sub_clocks] -max [all_outputs]
+  set_dont_touch_network [list $sub_clocks $sub_resets]
+  set_drive    0         [list $sub_clocks $sub_resets]
+  set_max_area 0
+  set_wire_load_mode enclosed
+  set_fix_multiple_port_nets -buffer_constants -all
+  compile -map_effort low
 
-remove_unconnected_ports [find -hierarchy cell {"*"}]
-write -format ddc -hierarchy -output $active_design.ddc
-write -format verilog -hierarchy -output $active_design.sv
+  # Export the mapped design
+  remove_unconnected_ports [find -hierarchy cell {"*"}]
+  set_dont_touch current_design
+  write -format ddc -hierarchy -output $active_design.ddc
+  write -format verilog -hierarchy -output $active_design.sv
 
-# Report area and timing
+  # Report area and timing
+  report_area -hierarchy > report_${active_design}_area.rpt
+  report_timing > report_${active_design}_timing.rpt
+  report_constraint -all_violators > report_${active_design}_constraint.rpt
 
-report_area -hierarchy > report_area.rpt
-report_timing > report_timing.rpt
-report_constraint -all_violators > report_constraint.rpt
+}
 
 quit
 
